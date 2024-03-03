1d67dc5a11f1fec98d10c3c19c80ac68de25bdd6
Jahid4b <jahid15-4361@diu.edu.bd>
2024-03-03 06:12:24 +0600

    ifelse added

---
+++ bin/ifelse.dart
@@ -0,0 +1,10 @@
+void main() {
+  bool isWeekend = false;
+  String weather = "sunny";
+
+  if (isWeekend && weather == "sunny") {
+    print("Let's go to the beach!");
+  } else {
+    print("Let's go to work!");
+  }
+}
